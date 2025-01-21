9fbc1f34a10dd05cdd92d288d6d2ad47a7d33d8c
Anya <zhangyazhen@zhangyahendeMBP.home>
2025-01-21 20:24:39 +0800

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/offcanvas';
+import 'bootstrap/js/dist/popover';
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/toast';
